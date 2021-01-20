f4f78eaa53c51f58757f1f89e0f47a8edcfc0e6f
buyf <704818424@qq.com>
2021-01-20 17:24:59 +0800

    整合mybatis plus

--- activiti/src/main/java/com/buyf/activiti/ActivitiApplication.java
+++ activiti/src/main/java/com/buyf/activiti/ActivitiApplication.java
@@ -4,11 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication(exclude ={SecurityAutoConfiguration.class} )
-
-@ImportResource(value = {"classpath*:mappers/*.xml"})
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class
+})
 @MapperScan("com.buyf.activiti.dao")
 public class ActivitiApplication {
 
